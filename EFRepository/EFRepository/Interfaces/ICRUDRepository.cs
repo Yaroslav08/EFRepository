8806c4cdf82eb066bd213aee34953968b0a4817b
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-26 19:44:00 +0200

    Added ICRUDRepository

---
+++ EFRepository/EFRepository/Interfaces/ICRUDRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFRepository.Interfaces
+{
+    public interface ICRUDRepository<TEntity, TypeId> where TEntity : class
+    {
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> RemoveAsync(TEntity entity);
+        Task<TEntity> GetByIdAsync(TypeId Id);
+        Task<TEntity> GetByIdAsTrackingAysnc(TypeId Id);
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> FirstAsync();
+        Task<TEntity> LastAsync();
+        Task<int> CountAsync();
+        Task<long> CountLongAsync();
+        Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> match);
+        Task<TEntity> FindAsTrackingAsync(Expression<Func<TEntity, bool>> match);
+        Task<List<TEntity>> FindListAsync(Expression<Func<TEntity, bool>> match);
+    }
+}
